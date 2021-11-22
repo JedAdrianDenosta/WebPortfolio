0403846c33640f17cdc4e6f5496ef6c7cc905aab
JedAdrianDenosta <Jadriandenosta@wvsu.edu.ph>
2021-11-22 19:54:35 +0800

    minor change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,27 @@
 module.exports = {
   purge: {
     mode: "jit",
-    mode: 'layers',
-    layers: ['base','components', 'utilities'],
-    content: ['./public/**/*.html'],
+    mode: "layers",
+    layers: ["base", "components", "utilities"],
+    content: ["./public/**/*.html"],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      colors:{
-        cwc:{
-          red:'#E84977',
-          blue:'#49AADE',
-          gray:'#222222',
-        }
-        
+      colors: {
+        cwc: {
+          red: "#E84977",
+          blue: "#49AADE",
+          gray: "#222222",
+        },
       },
       fontFamily: {
-        body: ['IBM Plex Sans']
-      }
+        body: ["IBM Plex Sans"],
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
